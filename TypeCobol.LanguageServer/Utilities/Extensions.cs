9f5c8e3f5b9d1848fd9df1bc263b6d9e53a22aff
mayanje <jean.mayan@externe.e-i.com>
2018-03-03 13:15:03 +0100

    #849: Making LSR test pass with server -tsource options, mapping LsrTestOptions to their corresponding TypeCobol.ExecutionStep options, thus adapting CompileOnce() method.

---
+++ TypeCobol.LanguageServer/Utilities/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCobol.LanguageServer.Utilities
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Extension method to get the TypeCobol.ExecutionStep value corresponding to a Workspace.LsrTestingOption value.
+        /// </summary>
+        /// <param name="lsrOptions">The lsr testing option value</param>
+        /// <param name="defaultValue">The default TypeCobol.ExecutionStep instance value</param>
+        /// <returns>The corresponding TypeCobol.ExecutionStep if any, null otherwise</returns>
+        public static TypeCobol.ExecutionStep? ExecutionStep(this Workspace.LsrTestingOptions lsrOptions, TypeCobol.ExecutionStep defaultValue)
+        {
+            switch (lsrOptions)
+            {
+                case Workspace.LsrTestingOptions.NoLsrTesting:
+                    return defaultValue;
+                case Workspace.LsrTestingOptions.LsrSourceDocumentTesting:
+                    return null;
+                case Workspace.LsrTestingOptions.LsrScanningPhaseTesting:
+                    return TypeCobol.ExecutionStep.Scanner;
+                case Workspace.LsrTestingOptions.LsrPreprocessingPhaseTesting:
+                    return TypeCobol.ExecutionStep.Preprocessor;
+                case Workspace.LsrTestingOptions.LsrParsingPhaseTesting:
+                    return TypeCobol.ExecutionStep.SyntaxCheck;
+                case Workspace.LsrTestingOptions.LsrSemanticPhaseTesting:
+                    return TypeCobol.ExecutionStep.CrossCheck;
+            }
+            return defaultValue;
+        }
+    }
+}
